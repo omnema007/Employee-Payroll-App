963f4b7442c2b617dbc0c19a2677d8b1f3da9062
Om Nema <134232774+omnema007@users.noreply.github.com>
2025-02-28 16:48:39 +0530

    [Om] Add. Ability to develop rest of the CRUD Service Methods with MySQL Database

--- src/main/java/com/springboot/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/springboot/employeepayrollapp/controller/EmployeePayrollController.java
@@ -7,6 +7,9 @@ import com.springboot.employeepayrollapp.service.EmployeePayrollService;
 import org.springframework.beans.factory.annotation.Autowired;
 import lombok.extern.slf4j.Slf4j;
 
+
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequestMapping("/employeepayrollservice")
@@ -14,6 +17,18 @@ public class EmployeePayrollController {
 
     @Autowired
     private EmployeePayrollService employeePayrollService;
+
+    @GetMapping("/getAll")
+    public List<EmployeePayroll> getAllEmployees() {
+        log.info("Received request to fetch all employees.");
+        return employeePayrollService.getAllEmployees();
+    }
+
+    @GetMapping("/get/{id}")
+    public EmployeePayroll getEmployeeById(@PathVariable int id) {
+        log.info("Received request to fetch employee with ID: {}", id);
+        return employeePayrollService.getEmployeeById(id);
+    }
 
     @PostMapping("/create")
     public EmployeePayroll createEmployee(@Valid @RequestBody EmployeePayrollDTO dto) {
@@ -25,6 +40,12 @@ public class EmployeePayrollController {
     public EmployeePayroll updateEmployee(@PathVariable int id, @Valid @RequestBody EmployeePayrollDTO dto) {
         log.info("Received request to update employee ID: {} with data: {}", id, dto);
         return employeePayrollService.updateEmployee(id, dto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        log.info("Received request to delete employee with ID: {}", id);
+        return employeePayrollService.deleteEmployee(id);
     }
 }
 
--- src/main/java/com/springboot/employeepayrollapp/service/EmployeePayrollService.java
+++ src/main/java/com/springboot/employeepayrollapp/service/EmployeePayrollService.java
@@ -1,6 +1,7 @@
 package com.springboot.employeepayrollapp.service;
 
 import com.springboot.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.springboot.employeepayrollapp.exception.EmployeeNotFoundException;
 import com.springboot.employeepayrollapp.model.EmployeePayroll;
 import org.springframework.stereotype.Service;
 import com.springboot.employeepayrollapp.repository.EmployeePayrollRepository;
@@ -8,12 +9,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
+
+import java.util.List;
+
 @Slf4j
 @Service
 public class EmployeePayrollService {
 
     @Autowired
     private EmployeePayrollRepository employeePayrollRepository;
+
+    public List<EmployeePayroll> getAllEmployees() {
+        log.info("Fetching all employees.");
+        return employeePayrollRepository.findAll();
+    }
+
+    public EmployeePayroll getEmployeeById(int id) {
+        log.info("Fetching employee with ID: {}", id);
+        return employeePayrollRepository.findById(id)
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found."));
+    }
 
     public EmployeePayroll createEmployee(@Valid EmployeePayrollDTO dto) {
         log.info("Creating Employee: {}", dto);
@@ -24,7 +39,7 @@ public class EmployeePayrollService {
     public EmployeePayroll updateEmployee(int id, @Valid EmployeePayrollDTO dto) {
         log.info("Updating Employee ID: {} with data: {}", id, dto);
         EmployeePayroll employee = employeePayrollRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Employee not found"));
+                .orElseThrow(() -> new EmployeeNotFoundException("Employee with ID " + id + " not found."));
         employee.setName(dto.getName());
         employee.setGender(dto.getGender());
         employee.setDepartment(dto.getDepartment());
@@ -33,6 +48,15 @@ public class EmployeePayrollService {
         employee.setNote(dto.getNote());
         employee.setProfilePic(dto.getProfilePic());
         return employeePayrollRepository.save(employee);
+    }
+
+    public String deleteEmployee(int id) {
+        log.info("Deleting Employee ID: {}", id);
+        if (!employeePayrollRepository.existsById(id)) {
+            throw new EmployeeNotFoundException("Employee with ID " + id + " not found.");
+        }
+        employeePayrollRepository.deleteById(id);
+        return "Employee with ID " + id + " deleted.";
     }
 }
 
